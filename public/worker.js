fa3719a5a15589f8482d822cdebc9864e0d9efe6
tristanbeykusuma <tristanbeykusuma@gmail.com>
2022-12-10 01:19:51 +0800

    Implement PWA functionality and caching

---
+++ public/worker.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = 'runding-web';
+const urlsToCache = [
+  '/',
+  '/ruang',
+  '/about',
+];
+
+// Install a service worker
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      }),
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }),
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['runding-web'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      // eslint-disable-next-line array-callback-return, consistent-return
+      cacheNames.map((cacheName) => {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      }),
+    )),
+  );
+});
